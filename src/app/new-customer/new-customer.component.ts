428bf426907d5481d3af8dba48e72d14f653ced8
mrbenboyyy <mrbenboyy@gmail.com>
2025-01-12 20:42:23 +0100

    added_role_input_to_add_form_and_list_form

--- src/app/new-customer/new-customer.component.ts
+++ src/app/new-customer/new-customer.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {Customer} from "../model/customer.model";
-import {CustomerService} from "../services/customer.service";
-import {Router} from "@angular/router";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CustomerService } from '../services/customer.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-customer',
@@ -10,26 +9,34 @@ import {Router} from "@angular/router";
   styleUrls: ['./new-customer.component.css']
 })
 export class NewCustomerComponent implements OnInit {
-  newCustomerFormGroup! : FormGroup;
-  constructor(private fb : FormBuilder, private customerService:CustomerService, private router:Router) { }
+  newCustomerFormGroup!: FormGroup;
+  availableRoles: string[] = ['ADMIN', 'USER'];
+
+  constructor(
+    private fb: FormBuilder,
+    private customerService: CustomerService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
-    this.newCustomerFormGroup=this.fb.group({
-      name : this.fb.control(null, [Validators.required, Validators.minLength(4)]),
-      email : this.fb.control(null,[Validators.required, Validators.email]),
-      password: ['', Validators.required]
+    this.newCustomerFormGroup = this.fb.group({
+      name: [null, [Validators.required, Validators.minLength(4)]],
+      email: [null, [Validators.required, Validators.email]],
+      role: ['USER', [Validators.required]], // Rôle par défaut
+      password: ['', [Validators.required]]
     });
   }
 
   handleSaveCustomer() {
-    let customer:Customer=this.newCustomerFormGroup.value;
+    if (this.newCustomerFormGroup.invalid) return;
+
+    const customer = this.newCustomerFormGroup.value;
     this.customerService.saveCustomer(customer).subscribe({
-      next : data=>{
-        alert("Customer has been successfully saved!");
-        //this.newCustomerFormGroup.reset();
-        this.router.navigate(["/admin/customers"]);
+      next: () => {
+        alert('Customer has been successfully saved!');
+        this.router.navigateByUrl('/admin/customers');
       },
-      error : err => {
+      error: (err) => {
         console.log(err);
       }
     });
